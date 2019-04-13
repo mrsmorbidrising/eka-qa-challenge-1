6476a9b8c9d32bb83e1c9d2bdcfd8d3ecaa0b923
mrsmorbidrising <49575826+mrsmorbidrising@users.noreply.github.com>
2019-04-12 19:40:46 -0700

    Updated

--- tests/navigate_to_site.js
+++ tests/navigate_to_site.js
@@ -1,10 +1,13 @@
 const globals = require('./settings/globals.js');
-
-module.exports = {
+yarn remove dep
+module.exports = function (tahtiana, brown)
   'Navigates To The EKA Site': (browser) => {
     return browser
       .url(globals.url)
       .waitForElementVisible('.nav-logo')
-      .end();
-  }
-}
+    .("#navbar_contactus_btn").on("click",function(e){
+    "how can we help" ();
+     (#join-mailinglist_input) 
+        .on ("click",function(e){#join-mailinglist_btn}
+     $('Request a demo').modal(options)
+      .end()
